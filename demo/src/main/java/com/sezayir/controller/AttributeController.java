551673bb9cc1bcd8a77cf0851e311ac12764365c
sezayirdagtekin <sezayir@hotmail.com>
2020-06-12 08:00:19 +0300

    fix CORS error

--- demo/src/main/java/com/sezayir/controller/AttributeController.java
+++ demo/src/main/java/com/sezayir/controller/AttributeController.java
@@ -4,12 +4,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.sezayir.model.Attribute;
 import com.sezayir.service.AttributeService;
 
+
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class AttributeController {
