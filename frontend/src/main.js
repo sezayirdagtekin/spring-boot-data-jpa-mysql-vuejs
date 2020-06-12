ac813cd1c8a56a5fae588a18aae2c867657b6223
sezayirdagtekin <sezayir@hotmail.com>
2020-06-12 08:23:36 +0300

    install axios for service call

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,15 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
+//install  axios
+import axios from 'axios'
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:8081/api"
+});
+
+Vue.prototype.$axios = axiosInstance;
+//Vue.prototype.$http = axios
+
 import VueRouter from 'vue-router'
 import routes from './routes';
 
